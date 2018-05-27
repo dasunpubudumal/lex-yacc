cbf9e6729f5229e06c14ee01c896bc9aded60d3e
heshan <heshan.15@cse.mrt.ac.lk>
2018-05-27 18:22:30 +0530

    final project

--- src/testfile.c
+++ src/testfile.c
@@ -1,1 +1,12 @@
-/* HELLO */
+/* A program to perform Euclid's
+Algorithm to compute gcd. */
+int gcd (int u, int v){
+if(v==0) return u ;
+else return gcd(v, u-u/v*v);
+/* u-u/v*v == u mod v*/
+}
+void main(void) {
+int x; int y;
+x = input(); y = input();
+output(gcd(x, y));
+}
